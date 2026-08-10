36c653fe8177daaf6346945f8afcfd0c2c106f26
rortyk67 <ringowotaberarenai@gmail.com>
2026-08-10 12:04:52 +0900

    クロージャーの練習を追加

--- basics/basic.py
+++ basics/basic.py
@@ -1,5 +1,11 @@
-while True:
-    word = input('Enter: ')
-    if word == 'ok':
-        break
-    print('next')
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+
+    return circle_area
+
+ca1 = circle_area_func(3.14)
+ca2 = circle_area_func(3.141592)
+
+print(ca1(10))
+print(ca2(10))
