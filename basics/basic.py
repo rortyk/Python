fd74b2883db9c340ba5c89cc755e1d14e97762ef
rortyk <ringowotaberarenai@gmail.com>
2026-08-29 21:08:48 +0900

    ダックタイピングの練習について追加

--- basics/basic.py
+++ basics/basic.py
@@ -1,9 +1,38 @@
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+
+    def drive(self):
+        if self.age >= 18:
+            print('OK')
+        else:
+            raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+baby = Baby()
+adult = Adult()
 class Car(object):
     def __init__(self, model=None):
         self.model = model
     def run(self):
         print('run')
+    def ride(self, person):
+        person.drive()
 
+car = Car()
+car.ride(adult)
 class ToyotaCar(Car):
     pass
 
@@ -33,5 +62,5 @@ class TeslaCar(Car):
     def auto_run(self):
         print('auto run')
 
-tesla_car = TeslaCar('Model S', passowd='111')
-tesla_car.run()
+# tesla_car = TeslaCar('Model S', passwd='111')
+# tesla_car.run()
