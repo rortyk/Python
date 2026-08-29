3b8b458dffc44aa64315c6fc1750167bc2c6a3a5
rortyk <ringowotaberarenai@gmail.com>
2026-08-29 20:45:17 +0900

    Propertyについての練習を追加

--- basics/basic.py
+++ basics/basic.py
@@ -8,24 +8,30 @@ class ToyotaCar(Car):
     pass
 
 class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=False):
+    def __init__(self, model='Model S', 
+                 enable_auto_run=False, 
+                 passwd = '123'):
         # self.model = model
         super().__init__(model)
-        self.enable_auto_run = enable_auto_run
-        
+        self.__enable_auto_run = enable_auto_run
+        self.passwd = passwd
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
+
     def run(self):
+        print(self.__enable_auto_run)
         print('super fast')
     def auto_run(self):
         print('auto run')
 
-car = Car()
-car.run()
-
-toyota_car = ToyotaCar('Lexus')
-print(toyota_car.model)
-toyota_car.run()
-
-tesla_car = TeslaCar('Model S')
-print(tesla_car.model)
+tesla_car = TeslaCar('Model S', passowd='111')
 tesla_car.run()
-tesla_car.auto_run()
