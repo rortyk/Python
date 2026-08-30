1b40f95147c1e4cb820fe4b86058cfec239ede8d
rortyk <ringowotaberarenai@gmail.com>
2026-08-30 12:35:57 +0900

    特殊メソッドの練習について追加

--- basics/basic.py
+++ basics/basic.py
@@ -1,66 +1,21 @@
-class Person(object):
-    def __init__(self, age=1):
-        self.age = age
+class Word(object):
 
-    def drive(self):
-        if self.age >= 18:
-            print('OK')
-        else:
-            raise Exception('No drive')
+    def __init__(self, text):
+        self.text = text
 
-class Baby(Person):
-    def __init__(self, age=1):
-        if age < 18:
-            super().__init__(age)
-        else:
-            raise ValueError
+    def __str__(self):
+        return 'Word!!!!!!'
 
-class Adult(Person):
-    def __init__(self, age=18):
-        if age >= 18:
-            super().__init__(age)
-        else:
-            raise ValueError
-baby = Baby()
-adult = Adult()
-class Car(object):
-    def __init__(self, model=None):
-        self.model = model
-    def run(self):
-        print('run')
-    def ride(self, person):
-        person.drive()
+    def __len__(self):
+        return len(self.text)
 
-car = Car()
-car.ride(adult)
-class ToyotaCar(Car):
-    pass
+    def __add__(self, word):
+        return self.text.lower() + word.text.lower()
 
-class TeslaCar(Car):
-    def __init__(self, model='Model S', 
-                 enable_auto_run=False, 
-                 passwd = '123'):
-        # self.model = model
-        super().__init__(model)
-        self.__enable_auto_run = enable_auto_run
-        self.passwd = passwd
+    def __eq__(self, word):
+        return self.text.lower() == word.text.lower()
 
-    @property
-    def enable_auto_run(self):
-        return self._enable_auto_run
+w = Word('test')
+w2 = Word('test')
 
-    @enable_auto_run.setter
-    def enable_auto_run(self, is_enable):
-        if self.passwd == '456':
-            self._enable_auto_run = is_enable
-        else:
-            raise ValueError
-
-    def run(self):
-        print(self.__enable_auto_run)
-        print('super fast')
-    def auto_run(self):
-        print('auto run')
-
-# tesla_car = TeslaCar('Model S', passwd='111')
-# tesla_car.run()
+print(w == w2)
