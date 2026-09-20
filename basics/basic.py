30391371beb68b742748be2c01ac89b37ff6afbc
rortyk <ringowotaberarenai@gmail.com>
2026-09-20 19:04:24 +0900

    特殊メソッドの練習について追記２

--- basics/basic.py
+++ basics/basic.py
@@ -1,21 +1,26 @@
-class Word(object):
+class Temperature(object):
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, value):
+        self.value = float(value)
+
+    def __eq__(self, tmp_value):
+        return self.value == tmp_value.value
 
     def __str__(self):
-        return 'Word!!!!!!'
+        return str(self.value) + '°C'
 
-    def __len__(self):
-        return len(self.text)
+    def __lt__(self, tmp_value):
+        return self.value < tmp_value.value
 
-    def __add__(self, word):
-        return self.text.lower() + word.text.lower()
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.value})'
 
-    def __eq__(self, word):
-        return self.text.lower() == word.text.lower()
+t1 = Temperature(25)
+t2 = Temperature(25.0)
+t3 = Temperature(-3.5)
 
-w = Word('test')
-w2 = Word('test')
-
-print(w == w2)
+print(t1)
+print(t1 == t2)
+print(t3 < t1)
+print([str(t) for t in sorted([t1, t3, Temperature(100)])])
+print(sorted([t1, t3, Temperature(100)]))
